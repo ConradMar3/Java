47aa27b3d9ffcb0f7df39f6b00c83a4f3b525564
ConradMar3 <conradmare890317@gmail.com>
2022-08-03 08:30:41 +0200

    2022/08/03

---
+++ Dummies/src/page12/HelloWorldApp.java
@@ -0,0 +1,14 @@
+package page12;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// Language: 'C and C++'
+/*
+main(){
+ printf("Hello, World!");
+}
+ */
