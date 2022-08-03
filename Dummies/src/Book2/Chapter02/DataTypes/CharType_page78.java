67028fc55528cde55c88e6988955495eca42a7a9
ConradMar3 <conradmare890317@gmail.com>
2022-08-03 17:24:32 +0200

    2022/08/03

---
+++ Dummies/src/Book2/Chapter02/DataTypes/CharType_page78.java
@@ -0,0 +1,9 @@
+package Book2.Chapter02.DataTypes;
+
+public class CharType_page78 {
+    public static void main(String[] args) {
+        // Use apostrophes ( ' ' ), not quotes ( " " ).
+        char code = 'X';
+        System.out.println(code);
+    }
+}
