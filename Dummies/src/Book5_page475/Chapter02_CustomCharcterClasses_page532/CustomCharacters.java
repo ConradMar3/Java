e92d04317c1f00d04f46a636e81af5a6f69b3a5a
ConradMar3 <conradmare890317@gmail.com>
2022-10-04 11:25:19 +0200

    2022/10/04 (am)
    Book05 - Chapter02.
    Regex.

--- Dummies/src/Book5_page475/Chapter02_CustomCharcterClasses_page532/CustomCharacters.java
+++ Dummies/src/Book5_page475/Chapter02_CustomCharcterClasses_page532/CustomCharacters.java
@@ -1,4 +1,4 @@
-package Book5_page475.CustomCharcterClasses_page532;
+package Book5_page475.Chapter02_CustomCharcterClasses_page532;
 
 //To create a custom character class, you simply list all the characters that you want
 //to include in the class within a set of brackets. Here’s an example:
