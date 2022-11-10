f2a60b066fd8751cee008fd2edad6b4018e4fb28
ConradMar3 <conradmare890317@gmail.com>
2022-11-10 10:09:29 +0200

    2022/11/10  (am)
    IntelliJ IDEA - Community version installed, run configurations has to be redone with new IDE, hence the commit.

--- Dummies/src/Book5_page475/Chapter04_CalculatingDates_page552/CalculateDifference.java
+++ Dummies/src/Book5_page475/Chapter04_CalculatingDates_page552/CalculateDifference.java
@@ -1,14 +1,13 @@
 package Book5_page475.Chapter04_CalculatingDates_page552;
 
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
-import java.time.*;
 
 public class CalculateDifference {
     public static void main(String[] args) {
 
-        LocalDate date1 = LocalDate.parse("2014-05-16");
-        LocalDate date2 = LocalDate.parse("2014-12-15");
+        LocalDate date1 = LocalDate.parse("2022-11-10");
+        LocalDate date2 = LocalDate.parse("2022-12-15");
         System.out.println(date1.until(date2, ChronoUnit.DAYS));
 
     }
--- Dummies/src/Book5_page475/Chapter04_ComparingDates_page552/isEqual.java
+++ Dummies/src/Book5_page475/Chapter04_ComparingDates_page552/isEqual.java
@@ -1,7 +1,6 @@
 package Book5_page475.Chapter04_ComparingDates_page552;
 
-import java.util.*;
-import java.time.*;
+import java.time.LocalDate;
 
 public class isEqual {
     public static void main(String[] args) {
@@ -9,7 +8,7 @@ public class isEqual {
         if (LocalDate.now().isEqual(LocalDate.now()))
             System.out.println("All is right in the universe.");
 
-        if (LocalDate.now().getDayOfMonth() < 15)
+        if (LocalDate.now().getDayOfMonth() < 5)
             System.out.println("It is not yet the 15th.");
     }
 }
