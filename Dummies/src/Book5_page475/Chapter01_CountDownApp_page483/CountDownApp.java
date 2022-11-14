e060f08cbd425547021a45218d1253882fb2d239
ConradMar3 <conradmare890317@gmail.com>
2022-11-14 10:34:52 +0200

    2022/11/15 (am)
    New package configurations.

--- Dummies/src/Book5_page475/Chapter01_CountDownApp_page483/CountDownApp.java
+++ Dummies/src/Book5_page475/Chapter01_CountDownApp_page483/CountDownApp.java
@@ -2,12 +2,17 @@ package Book5_page475.Chapter01_CountDownApp_page483;
 
 public class CountDownApp {
     public static void main(String[] args) {
+
         Thread clock = new CountDownClock();
+
         java.lang.Runnable flood, ignition, liftoff;
+
         flood = new LaunchEvent(16, "Flood the pad!");
         ignition = new LaunchEvent(6, "Start engines!");
         liftoff = new LaunchEvent(0, "Liftoff!");
+
         clock.start();
+
         new Thread(flood).start();
         new Thread(ignition).start();
         new Thread(liftoff).start();
